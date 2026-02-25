6371ce27a8098bb69e11665a80c363ca78b09be1
Fiercedarkvenom <db220605@gmail.com>
2026-02-24 18:27:14 -0800

    Import Update

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,13 @@
 package com.example;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
